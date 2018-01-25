f4606d1144ce0106ec4b2a5ebf724d7a6c6d8407
omar gentil manjarres forero <kofperfect922@hotmail.com>
2018-01-25 10:02:04 -0500

    Agregar archivos de proyecto.

---
+++ AuditoriaPorvenir/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuditoriaPorvenir.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Bienvenido";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string inputEmail, string inputPassword)
+        {
+            return View("Contact");
+        }
+    }
+}
